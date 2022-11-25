7aa771599fc1787e4ac1bf6082cdf03eb87f9da9
Gam13t <53529326+Gam13t@users.noreply.github.com>
2022-11-25 23:28:47 +0200

    Sheesh

--- va.py
+++ va.py
@@ -1,8 +1,10 @@
 # TODO: add logs
 # TODO: speed up a little
-
+from pathlib import Path
 import datetime
+import subprocess
 import webbrowser as browser_controller
+from playsound import playsound
 from providers import TTSEngineProvider, tts_engine, speech_recognition_provider
 from constants import PredefinedVocabularity, action_linker
 from config import filled_config as config
@@ -17,23 +19,56 @@ class VAProviderHelper:
         self.speech_frame = speech_recognition_provider
         self.config = config
 
+    def play(self, audio_file_path="acdc-back-in-black.wav"):
+        current_path = Path(__file__).parent.resolve()
+        sound_path = str(current_path / "sounds" / "acdc-back-in-black.wav")
+        playsound(sound_path, block=False)
+
 
 class VACommandsRealisation(VAProviderHelper):
+    def response(func):
+        def wrap(*args, **kwargs):
+            result = func(*args, **kwargs)
+            tts_engine.speak("Слушаюсь, хозяин")
+            return result
+
+        return wrap
+
     def current_time(self):
         now = datetime.datetime.now()
         self.tts_engine.speak("Сейчас " + str(now.hour) + ":" + str(now.minute))
 
+    @response
     def open_google(self):
-        browser_controller.open("https://www.google.co.uk")
+        browser_controller.open("https://www.google.com")
 
+    @response
     def open_command_communications(self):
         browser_controller.open(
             "https://m.pantheon.sk/mattermost/messages/@maksym.vynnyk"
         )
         browser_controller.open("https://mail.pantheon.sk/owa/")
 
+    @response
     def open_apple_music(self):
         browser_controller.open("https://music.apple.com/us/browse")
+
+    @response
+    def sadness(self):
+        browser_controller.open("https://www.youtube.com/watch?v=rcGewMgY8Sw")
+
+    @response
+    def working_protocol(self):
+        path_to_vscode = (
+            "C://Users//38099//AppData//Local//Programs//Microsoft VS Code//Code.exe"
+        )
+        self.play()
+        self.open_command_communications()
+        self.open_google()
+        subprocess.call(path_to_vscode)
+
+    def joke_question(self):
+        self.tts_engine.speak("Я ваша рабыня, господин!")
 
 
 class VAExecutor(VACommandsRealisation):
@@ -59,6 +94,9 @@ class VAExecutor(VACommandsRealisation):
             "open_google": self.open_google,
             "open_command_communications": self.open_command_communications,
             "open_apple_music": self.open_apple_music,
+            "sadness": self.sadness,
+            "working_protocol": self.working_protocol,
+            "joke_question": self.joke_question,
         }
 
 
@@ -108,5 +146,6 @@ class VAInterface(VAExecutor):
                     SUGGESTED_COMMAND["command"] = action
                     SUGGESTED_COMMAND["percent"] = percent_similarities
                     SUGGESTED_COMMAND["action"] = key
+                print(SUGGESTED_COMMAND)
 
         self.execute_command_by_link(SUGGESTED_COMMAND["action"])
